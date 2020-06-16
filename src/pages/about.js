26d64e1191f77ea73ce42e087e1adf2ed01c21b8
hendrixmar <hendrikuwabara@gmail.com>
2020-06-16 17:29:04 -0500

    deployed to surge

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
+      <Header headerText="bruh"/>
       <p>Such wow. Very React.</p>
     </div>
   )
